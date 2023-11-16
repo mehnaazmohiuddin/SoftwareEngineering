57914881d0d2619b9007c6c7970f91ff3927b4ab
mehnaazmohiuddin <mehnaazmohiuddin@gmail.com>
2023-11-16 11:47:56 +0530

    adding docker compose and changing the db to postgres from h2

--- Make-A-Donation/src/main/resources/schema.sql
+++ Make-A-Donation/src/main/resources/schema.sql
@@ -1,17 +1,14 @@
-create table if not exists Charity
+create table if not exists charity
 (
-  id INTEGER AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY ,
   name VARCHAR(255) NOT NULL,
   orgnaization VARCHAR(255) ,
   amount INTEGER,
   currency VARCHAR(3),
-  date INTEGER,
-  city VARCHAR(45),
-  primary key (id)
+  date date,
+  city VARCHAR(45)
 );
 
-alter table Charity
-alter column date date;
-insert into Charity( name, orgnaization, amount, currency, date, city) values ('mehnaaz','faiz-e-aam', 2000,'inr', '2017-07-14','hyderabad');
+insert into charity( name, orgnaization, amount, currency, date, city) values ('mehnaaz','faiz-e-aam', 2000,'inr', '2017-07-14','hyderabad');
 
-select * from Charity;
+select * from charity;
